3eadf7260c4ff5b1434b446f6d5659044362cbc0
uellenberg <JonahUellenberg@hotmail.com>
2022-05-10 01:43:36 -0700

    Start intro to p5

---
+++ sections/intro_to_p5/0_what_is_p5/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+const getFile = (file) => (req, res) => res.send(fs.readFileSync(file, "utf-8"));
+app.get("/", getFile("index.html"));
+app.get("/sketch.js", getFile("user/sketch.js"))
+
+app.listen(3000);
---
+++ sections/intro_to_p5/0_what_is_p5/user/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(100);
+    ellipse(mouseX, mouseY, 50, 50);
+}
