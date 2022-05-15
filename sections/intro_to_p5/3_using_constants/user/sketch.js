d157665d16200b1c046b537ace9facc364e15206
uellenberg <JonahUellenberg@hotmail.com>
2022-05-14 23:22:47 -0700

    Add new p5.js lesson

---
+++ sections/intro_to_p5/3_using_constants/user/sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(100);
+
+    // This looks like a sweet spot to put a constant.
+
+    // Here's our big rectangle.
+    rect(50, 75, 100, 80);
+
+    // Here are the smaller ones.
+    //
+    // Keep in mind that not all numbers need to be constants.
+    // You should only make the x, y, width, and height constants.
+    // Everything else (2, 3, 4) are numbers that manipulate them and can
+    // stay as-is.
+    rect(50 - 100 / 4, 75 - 80 / 4, 100 / 2, 75 / 2);
+    rect(50 + (3 * 100) / 4, 75 - 80 / 4, 100 / 2, 75 / 2);
+    rect(50 - 100 / 4, 75 + (3 * 80) / 4, 100 / 2, 75 / 2);
+    rect(50 + (3 * 100) / 4, 75 + (3 * 80) / 4, 100 / 2, 75 / 2);
+}
