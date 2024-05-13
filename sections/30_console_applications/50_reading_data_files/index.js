88539ec50e38b947d144d38947bf7e099e6bd782
uellenberg <JonahUellenberg@hotmail.com>
2024-05-13 14:25:56 -0700

    Add descriptions

--- sections/30_console_applications/50_reading_data_files/index.js
+++ sections/30_console_applications/50_reading_data_files/index.js
@@ -1,1 +1,1 @@
-// TOD: Read in data.txt, process it (so it's an array of numbers), then print that array to the console.
+// TODO: Read in data.txt, process it (so it's an array of numbers), then print that array to the console.
